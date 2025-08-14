ad0b4b50b020bb6d7aaf55a40a63822d5c9915d4
serhiohikaru <srgschein@gmail.com>
2025-08-14 07:22:51 +0200

    Update 2015_0Update 2015_011

--- 2015-01/main.c
+++ 2015-01/main.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
+#include <stdint.h>
 
 int main()
 {
-  /* I need to input the file */
+  int result;
+  const char* fname = "./input.txt";
+  const char* up_char = "(";
+  const char* down_char = ")";
+  FILE* input = fopen(fname, "r");
+  if( !input )
+  {
+    perror("Could not open file!");
+  }
 
-  printf("hello");
+  int c; // note: int, not char, required to handle EOF
+  while ((c = fgetc(input)) != EOF) // standard C I/O file reading loop
+    {
+      if( (uint8_t)c == up_char[0]) result = result + 1;
+      if( (uint8_t)c == down_char[0]) result = result - 1;
+    }
+
+  printf( "%i", result );
+  return 0;
 }
