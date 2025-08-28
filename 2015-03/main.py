b17c0a265e68ecc523b1b40644c8be9c623da4c5
serhiohikaru <srgschein@gmail.com>
2025-08-28 22:50:40 +0200

    Update 2015-03 part two

--- 2015-03/main.py
+++ 2015-03/main.py
@@ -1,19 +1,40 @@
-x = 50
-y = 50
-result_dic = {"5050": 1}
-with open("./input.txt", "r") as f:
+import itertools
+x_santa = 200
+x_robot = 200
+y_santa = 200
+y_robot = 200
+result_dic = {"200200": 2}
+with open("./input_test.txt", "r") as f:
     input = f.read()
     for char in input:
-        if char == "<":
-            y = y - 1
-        elif char == ">":
-            y = y + 1
+        if char == "v":
+            print(char)
+            y_santa = y_santa - 1
+            y_robot = y_robot + 1
         elif char == "^":
-            x = x + 1
-        elif char == "v":
-            x = x - 1
-        key = str(x) + str(y)
-        if not key in result_dic:
-            result_dic.update({key: 0})
-        result_dic[key] += 1
+            print(char)
+            y_santa = y_santa + 1
+            y_robot = y_robot - 1
+        elif char == ">":
+            print(char)
+            x_santa = x_santa + 1
+            x_robot = x_robot - 1
+        elif char == "<":
+            print(char)
+            x_santa = x_santa - 1
+            x_robot = x_robot + 1
+        key_santa = str(x_santa) + str(y_santa)
+        key_robot = str(x_robot) + str(y_robot)
+        if key_santa == key_robot:
+            if not key_santa in result_dic:
+                result_dic.update({key_santa: 0})
+            result_dic[key_santa] += 1
+        else:
+            for key in (key_santa, key_robot):
+                print(key_santa)
+                print(key_robot)
+                if not key in result_dic:
+                    result_dic.update({key: 0})
+                result_dic[key] += 1
     print(len(result_dic))
+    print(result_dic)
